d7812472a65bd90b368b3d96c764987c5ae562f9
Muhamad Ridwan <ridwanwan784@gmail.com>
2022-04-02 22:20:22 +0700

    Initial commit

--- app/src/main/java/com/example/projectridwan/LoginActivity.java
+++ app/src/main/java/com/example/projectridwan/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.projectridwan;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
--- app/src/main/java/com/example/projectridwan/SplashScreenActivity.java
+++ app/src/main/java/com/example/projectridwan/SplashScreenActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static final int e = 954;
+    private static final int e = 585;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
