63cd5d016a1c41a4969f95aeec56a0e5f0a8933b
Muhamad Ridwan <ridwanwan784@gmail.com>
2022-04-06 17:40:26 +0700

    Initial commit

--- app/src/main/java/com/example/projectridwan/SplashScreenActivity.java
+++ app/src/main/java/com/example/projectridwan/SplashScreenActivity.java
@@ -2,14 +2,22 @@ package com.example.projectridwan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static final int e = 585;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 3000l);
     }
 }
